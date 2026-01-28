ccaec57786bda7db5d4f978d72097fc38438bdf4
luismanresaramirez-ops <luismanresaramirez@gmail.com>
2026-01-28 23:21:29 +0100

    Add FormsModule for ngModel bindings

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ArticleCreateComponent } from './components/article-create/article-create.component';
+import { ArticleDetailComponent } from './components/article-detail/article-detail.component';
+import { ArticleLinkComponent } from './components/article-link/article-link.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+import { MoisGratuitsComponent } from './components/mois-gratuits/mois-gratuits.component';
+import { PromotionCheckComponent } from './components/promotion-check/promotion-check.component';
+import { PromotionListComponent } from './components/promotion-list/promotion-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleCreateComponent,
+    ArticleDetailComponent,
+    ArticleLinkComponent,
+    ArticleListComponent,
+    MoisGratuitsComponent,
+    PromotionCheckComponent,
+    PromotionListComponent
+  ],
+  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,6 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { config } from './app/app.config.server';
+import { platformServer } from '@angular/platform-server';
+import { AppServerModule } from './app/app.server.module';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = () => platformServer().bootstrapModule(AppServerModule);
 
 export default bootstrap;
