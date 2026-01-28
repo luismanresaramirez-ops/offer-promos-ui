792ff62f4230ea09f2ca1db61111f27d73aa2cf6
luismanresaramirez-ops <luismanresaramirez@gmail.com>
2026-01-28 23:54:15 +0100

    Restyle header without Material deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -27,15 +24,7 @@ import { PromotionListComponent } from './components/promotion-list/promotion-li
     PromotionCheckComponent,
     PromotionListComponent
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    FormsModule,
-    AppRoutingModule,
-    MatButtonModule,
-    MatToolbarModule
-  ],
+  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
